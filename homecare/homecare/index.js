25c826c5074637f3ddd9533b24fe98b176b4c021
WILLIAMXAVIERR <williamrego203@gmail.com>
2025-08-26 21:20:39 -0300

    Atualizando as pastas dos arquivos

--- homecare/homecare/index.js
+++
@@ -1,4 +0,0 @@
-// Botão de inscrição do curso
-document.getElementById("btnInscricao").addEventListener("click", function() {
-  alert("Obrigado pelo interesse! Nossa equipe entrará em contato em breve.");
-});
---
+++ homecare/index.js
@@ -0,0 +1,4 @@
+// Botão de inscrição do curso
+document.getElementById("btnInscricao").addEventListener("click", function() {
+  alert("Obrigado pelo interesse! Nossa equipe entrará em contato em breve.");
+});
